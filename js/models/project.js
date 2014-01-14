3717a656ff397e726ba1302588860ca432fae692
Evrard Boulou <evrard@octoplus.co.za>
2014-01-14 10:55:16 +0200

    initialising client manager directory

---
+++ js/models/project.js
@@ -0,0 +1,11 @@
+﻿define([
+  'underscore',
+  'backbone'
+], function(_, Backbone){
+  var ProjectModel = Backbone.Model.extend({
+    defaults: {
+      name: "Harry Potter"
+    }
+  });
+  return ProjectModel;
+});
